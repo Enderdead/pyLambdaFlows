ab7fb86a269a148ae1760102b22736ec12efed92
Gauthier-Clerc François <francois@gauthier-clerc.fr>
2019-11-14 09:54:33 +0100

    replace deprecated credential providing.

--- example.py
+++ example.py
@@ -14,12 +14,12 @@ b = pyLambdaFlows.op.Map(a, "./source/compute.py")
 c = pyLambdaFlows.op.Reduce(b, "./source/mean.py")
 json = None
 
-with pyLambdaFlows.Session(aws_access_key_id="AKIA2IEN5IAPGJRZQAMQ", aws_secret_access_key="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") as sess:
+with pyLambdaFlows.Session(credentials_csv="./accessKeys.csv") as sess:
     c.compile(purge=False)
     result = c.eval(feed_dict={a:[1,2,3]})
     print(result)
     data = list()
-    for i in range(1000):
+    for i in range(100):
         data.append(randint(0,100))
     result = c.eval(feed_dict={a:data})
     print(result)
--- example2.py
+++ example2.py
@@ -1,8 +1,8 @@
 import pyLambdaFlows
 
-sess = pyLambdaFlows.Session(aws_access_key_id="AKIA2IEN5IAPGJRZQAMQ", aws_secret_access_key="qN1F8cgSLkJBmChR3Ht3KABQAqygNzY9sRy91X4G")
+sess = pyLambdaFlows.Session(credentials_csv="./accessKeys.csv")
 
 a = pyLambdaFlows.upload.Uploader(sess)
-a.upload_lambda("./test.py")
+a.upload_lambda("./source/mean.py")
 
 
