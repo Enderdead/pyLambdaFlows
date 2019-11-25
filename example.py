b4684f10436f88ad569795f0a0385d80b1025222
Gauthier-Clerc François <francois@gauthier-clerc.fr>
2019-11-25 11:09:53 +0100

    update sample with dynamodb

--- example.py
+++ example.py
@@ -1,12 +1,6 @@
 import pyLambdaFlows
 from random import randint
 
-def double(x):
-    return x*x
-
-def mean(liste):
-    return mean(liste)
-
 a = pyLambdaFlows.op.Source()
 
 b = pyLambdaFlows.op.Map(a, "./source/compute.py")
@@ -18,10 +12,4 @@ with pyLambdaFlows.Session(credentials_csv="./accessKeys.csv") as sess:
     c.compile(purge=False)
     result = c.eval(feed_dict={a:[1,2,3]})
     print(result)
-    data = list()
-    for i in range(100):
-        data.append(randint(0,100))
-    result = c.eval(feed_dict={a:data})
-    print(result)
     
-
--- example2.py
+++ example2.py
@@ -1,8 +1,18 @@
 import pyLambdaFlows
-
+import boto3
 sess = pyLambdaFlows.Session(credentials_csv="./accessKeys.csv")
 
-a = pyLambdaFlows.upload.Uploader(sess)
-a.upload_lambda("./source/mean.py")
+
+#pyLambdaFlows.DynamoGesture.delete_table("pyLambda", sess=sess)
+
+#pyLambdaFlows.DynamoGesture.create_table("pyLambda", sess=sess)
+#pyLambdaFlows.DynamoGesture.delete_table("pyLambda", sess=sess)
+print("fin")
+#pyLambdaFlows.DynamoGesture.delete_table("pyLambda", sess=sess)
+
+#pyLambdaFlows.DynamoGesture.fill_table("pyLambda", [1, 2, 3], sess=sess)
+res = pyLambdaFlows.DynamoGesture.decremente("pyLambda", 1, sess=sess)
+#a = boto3.resource("dynamodb", region_name=sess.region, aws_access_key_id=sess.aws_access_key_id, aws_secret_access_key= sess.aws_secret_access_key)
+#table = a.Table("pyLambda")
 
 
