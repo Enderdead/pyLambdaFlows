b30557c8d089650838a4240df1cde2396dd914f6
Francois Gauthier-Clerc <francois@gauthier-clerc.fr>
2019-11-30 16:01:30 +0100

    add put and get dynamobd utils func

--- pyLambdaFlows/DynamoGesture.py
+++ pyLambdaFlows/DynamoGesture.py
@@ -2,6 +2,7 @@ import botocore
 from .session import get_default_session
 import boto3
 import decimal
+import pickle
 
 def reset_table(table_name, sess=None):
     if sess is None:
@@ -92,6 +93,30 @@ def fill_table(table_name, counter_init, sess=None):
     for idx, init_val in enumerate(counter_init):
         client.put_item(TableName=table_name, Item={
             "id" : {"N" : str(idx)},
-            "remaining" : { "N" : str(init_val)}
+            "remaining" : { "N" : str(init_val)},
+            "data" : { "B" : pickle.dumps(0) }
         })
     return
+
+def put_data(table_name, indexData, obj, sess=None):
+
+    client = sess.getDynamoDbRessource()
+    table = client.Table(table_name)
+    response = table.update_item(Key={
+        'id' : indexData
+    },
+    UpdateExpression="set #data = :val",
+    ExpressionAttributeValues={
+        ':val' : pickle.dumps(obj)
+    },
+    ExpressionAttributeNames={
+    "#data": "data"
+    },
+    ReturnValues="UPDATED_NEW")
+
+
+def get_data(table_name, indexData, sess=None):
+    if sess is None:
+        sess = get_default_session()
+    client = sess.getDynamoDb()
+    return pickle.loads(client.get_item(TableName=table_name, Key= { "id" : {"N" : str(indexData)}})["Item"]["data"]["B"])
